e72d2222e21b98674fe5feb89216e69935e694b3
matveyprupas <matveyprupas@gmail.com>
2021-06-06 14:25:01 +0300

    fix: fix some mistakes afer review

--- js-classes-inheritance/script.js
+++ js-classes-inheritance/script.js
@@ -1,50 +1,33 @@
 class Builder {
-    constructor (arg) {
-        this.arg = arg;
+    constructor (accumulator) {
+        this.accumulator = accumulator;
     }
 
     plus(...n) {
-        for (let i = 0; i < arguments.length; i++) {
-            this.arg += arguments[i];
-        }
+        this.accumulator = this.accumulator + n.reduce((accum, currentVal) => accum + currentVal);
         return this;
     }
 
     minus(...n) {
-        if (typeof this.arg == "string") {
-            this.arg = this.arg.slice(0, this.arg.length - n);
-            return this;
-        }
-        for (let i = 0; i < arguments.length; i++) {
-            this.arg -= arguments[i];
-        }
+        this.accumulator = this.accumulator - n.reduce((accum, currentVal) => accum + currentVal);
+        // for (let i = 0; i < n.length; i++) {
+        //     this.accumulator -= n[i];
+        // }
         return this;
     }
 
     multiply(n) {
-        if (typeof this.arg == "string") {
-            let str = this.arg;
-            for (let i = 2; i <= n; i++) {
-                this.plus(str);
-            }
-            return this;
-        }
-        this.arg = this.arg*n;
+        this.accumulator = this.accumulator*n;
         return this;
     }
 
     divide(n) {
-        if (typeof this.arg == "string") {
-            let k = Math.floor(this.arg.length / n);
-            this.arg = this.arg.slice(0, k);
-            return this;
-        }
-        this.arg = this.arg/n;
+        this.accumulator = this.accumulator/n;
         return this;
     }
 
     get () {
-        return this.arg;
+        return this.accumulator;
     }
 }
 
@@ -55,14 +38,14 @@ class Builder {
 
 class IntBuilder extends Builder {
     mod(n) {
-        this.arg = this.arg%n;
+        this.accumulator = this.accumulator%n;
         return this;
     }
 }
 
 let intBuilder = new IntBuilder(10);
 
-console.log(intBuilder.plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).get());
+console.log(intBuilder.plus(2, 3, 2).minus(1,2).multiply(2).divide(4).mod(3).get());
 
 
 
@@ -70,25 +53,45 @@ console.log(intBuilder.plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).ge
 
 // Create StringBuilder on ES5
 
-function StringBuilder(arg) {
-    this.arg = arg;
-
-    this.remove = function(str) {
-        this.arg = this.arg.split(str).join("");
-        return this;
-    };
-
-    this.sub = function(from, n) {
-        this.arg = this.arg.substring(from,from+n);
-        return this;
-    };
+function StringBuilder(accumulator) {
+    this.accumulator = accumulator;
   }
 
 
 StringBuilder.prototype = Object.create(Builder.prototype);
 
+StringBuilder.prototype.minus = function(n) {
+    this.accumulator = this.accumulator.slice(0, this.accumulator.length - n);
+    return this;
+}
+
+StringBuilder.prototype.multiply = function(n) {
+    let str = this.accumulator;
+    for (let i = 2; i <= n; i++) {
+        this.plus(str);
+    }
+    return this;
+}
+
+StringBuilder.prototype.divide = function(n) {
+    let k = Math.floor(this.accumulator.length / n);
+    this.accumulator = this.accumulator.slice(0, k);
+    return this;
+}
+
+StringBuilder.prototype.remove = function(str) {
+    this.accumulator = this.accumulator.split(str).join("");
+    return this;
+  }
+
+StringBuilder.prototype.sub = function(from, n) {
+    this.accumulator = this.accumulator.substring(from,from+n);
+    return this;
+}
+
+
 
 let strBuilder = new StringBuilder("Hello");
 
-console.log(strBuilder.plus(" all", "!").minus(4).multiply(3).divide(4).remove("l").sub(1,1).get());
+console.log(strBuilder.plus(" all", "!").minus(4).multiply(3).divide(4).remove('l').sub(1,1).get());
 
